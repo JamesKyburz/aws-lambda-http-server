2f861e0a3273cb7236b1e3925c493a20ddeed371
James Kyburz <james.kyburz@gmail.com>
2018-01-24 15:05:18 +0100

    remove onRequest as lambda can add this functionality

--- src/index.js
+++ src/index.js
@@ -3,10 +3,7 @@
 const httpHandler = require('in-memory-http-listener')
 const createRequestResponse = require('aws-lambda-create-request-response')
 
-const proxy = (event, context, callback) => {
+module.exports = (event, context, callback) => {
   const { req, res } = createRequestResponse(event, callback)
-  if (proxy.onRequest) proxy.onRequest(req, res)
   httpHandler(process.env.PORT)(req, res)
 }
-
-module.exports = proxy
