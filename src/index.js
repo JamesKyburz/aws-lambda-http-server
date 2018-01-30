4ac256dab25dd16cc85aa5f1e0ecc53bb1f51156
James Kyburz <james.kyburz@gmail.com>
2018-01-30 23:19:50 +0100

    don't wait for event loop (prevent lambda timeout)

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const httpHandler = require('in-memory-http-listener')
 const createRequestResponse = require('aws-lambda-create-request-response')
 
 module.exports = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false
   const { req, res } = createRequestResponse(event, callback)
   httpHandler(process.env.PORT)(req, res)
 }
