0cdfb066af3a5909ce6aa32e082d71c80ef7e52c
James Kyburz <james.kyburz@gmail.com>
2018-03-31 14:47:29 +0200

    add warmup lambda check

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const httpHandler = require('in-memory-http-listener')
 const createRequestResponse = require('aws-lambda-create-request-response')
 
 module.exports = (event, context, callback) => {
+  if (event.source === 'serverless-plugin-warmup') {
+    return callback(null, 'Lambda is warm!')
+  }
   context.callbackWaitsForEmptyEventLoop = false
   const { req, res } = createRequestResponse(event, callback)
   httpHandler(process.env.PORT)(req, res)
