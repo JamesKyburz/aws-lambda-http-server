d1c9d89525f8f428f50fdcbbcf2ec05a9f4d0a18
James Kyburz <james.kyburz@gmail.com>
2018-04-04 21:25:29 +0200

    fix timeout callbackWaitsForEmptyEventLoop

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ const httpHandler = require('in-memory-http-listener')
 const createRequestResponse = require('aws-lambda-create-request-response')
 
 module.exports = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false
   if (event.source === 'serverless-plugin-warmup') {
     return callback(null, 'Lambda is warm!')
   }
-  context.callbackWaitsForEmptyEventLoop = false
   const { req, res } = createRequestResponse(event, callback)
   httpHandler(process.env.PORT)(req, res)
 }
