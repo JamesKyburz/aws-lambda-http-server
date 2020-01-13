beacc9b172d7fee9c3f3c1ad4e93081e3de7a24f
James Kyburz <james.kyburz@gmail.com>
2020-01-13 14:24:38 +0100

    added koa test

---
+++ packages/aws-lambda-http-server/test/koa.js
@@ -0,0 +1,70 @@
+require('./helpers/local-require')
+const { test } = require('tap')
+const handler = require('..')
+
+process.env.PORT = 1
+
+const Koa = require('koa')
+const app = new Koa()
+
+let onRequest = ctx => {}
+
+const event = { httpMethod: 'GET', requestContext: { path: '/' }, headers: {} }
+
+test('setup server', t => {
+  app.use(ctx => onRequest(ctx))
+  app.listen(process.env.PORT, () => t.end())
+})
+
+test('callbackWaitsForEmptyEventLoop yes', t => {
+  process.env.WAIT_FOR_EMPTY_EVENT_LOOP = 'yes'
+  const context = {}
+  handler(event, context, () => {
+    t.equals(true, context.callbackWaitsForEmptyEventLoop)
+    t.end()
+  })
+})
+
+test('callbackWaitsForEmptyEventLoop not yes', t => {
+  process.env.WAIT_FOR_EMPTY_EVENT_LOOP = 'xyes'
+  const context = {}
+  handler(event, context, () => {
+    t.equals(false, context.callbackWaitsForEmptyEventLoop)
+    t.end()
+  })
+})
+
+test('serverless-plugin-warmup', t => {
+  const warmup = { ...event, source: 'serverless-plugin-warmup' }
+  handler(warmup, {}, (err, result) => {
+    t.error(err)
+    t.equals('Lambda is warm!', result)
+    t.end()
+  })
+})
+
+test('ok response', t => {
+  onRequest = ctx => {
+    ctx.body = 'ok'
+  }
+  handler(event, {}, (err, result) => {
+    t.error(err)
+    t.equals('ok', result.body)
+    t.equals(200, result.statusCode)
+    t.end()
+  })
+})
+
+test('SERVER_PORT', t => {
+  delete process.env.PORT
+  process.env.SERVER_PORT = 1
+  onRequest = ctx => {
+    ctx.body = 'still ok'
+  }
+  handler(event, {}, (err, result) => {
+    t.error(err)
+    t.equals('still ok', result.body)
+    t.equals(200, result.statusCode)
+    t.end()
+  })
+})
