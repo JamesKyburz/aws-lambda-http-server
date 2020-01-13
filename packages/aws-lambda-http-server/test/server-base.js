fd491eda2c716ec8720bbe222c6748a371f89e91
James Kyburz <james.kyburz@gmail.com>
2020-01-13 14:37:32 +0100

    server-base tests

---
+++ packages/aws-lambda-http-server/test/server-base.js
@@ -0,0 +1,69 @@
+require('./helpers/local-require')
+const { test } = require('tap')
+const handler = require('..')
+
+process.env.PORT = 1
+process.env.LOG_LEVEL = 'error'
+
+const base = require('server-base')
+
+let onRequest = (req, res) => res.end()
+
+const event = { httpMethod: 'GET', requestContext: { path: '/' }, headers: {} }
+
+base({
+  '/': {
+    get (req, res) {
+      onRequest(res, res)
+    }
+  }
+}).start(process.env.PORT)
+
+test('callbackWaitsForEmptyEventLoop yes', t => {
+  process.env.WAIT_FOR_EMPTY_EVENT_LOOP = 'yes'
+  const context = {}
+  handler(event, context, () => {
+    t.equals(true, context.callbackWaitsForEmptyEventLoop)
+    t.end()
+  })
+})
+
+test('callbackWaitsForEmptyEventLoop not yes', t => {
+  process.env.WAIT_FOR_EMPTY_EVENT_LOOP = 'xyes'
+  const context = {}
+  handler(event, context, () => {
+    t.equals(false, context.callbackWaitsForEmptyEventLoop)
+    t.end()
+  })
+})
+
+test('serverless-plugin-warmup', t => {
+  const warmup = { ...event, source: 'serverless-plugin-warmup' }
+  handler(warmup, {}, (err, result) => {
+    t.error(err)
+    t.equals('Lambda is warm!', result)
+    t.end()
+  })
+})
+
+test('ok response', t => {
+  onRequest = (req, res) => res.end('ok')
+  handler(event, {}, (err, result) => {
+    t.error(err)
+    t.equals('ok', result.body)
+    t.equals(200, result.statusCode)
+    t.end()
+  })
+})
+
+test('SERVER_PORT', t => {
+  delete process.env.PORT
+  process.env.SERVER_PORT = 1
+  onRequest = (req, res) => res.end('still ok')
+  handler(event, {}, (err, result) => {
+    t.error(err)
+    t.equals('still ok', result.body)
+    t.equals(200, result.statusCode)
+    t.end()
+  })
+})
